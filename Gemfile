b76e216d728e85212f83606b3224961679cd80e3
Ulvis Goldbergs <ulvis.goldbergs@testdevlab.com>
2017-10-31 13:05:18 +0200

    Add all views

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+
+gem 'rake'
+
+gem 'sinatra', require: 'sinatra'
+gem 'thin', '~> 1.7'
+gem 'mongo', '~> 2.2', '>= 2.2.5'
+gem 'bson_ext'
---
+++ main.rb
@@ -0,0 +1,74 @@
+# Requires the Gemfile
+require 'bundler' ; Bundler.require
+set :bind, '0.0.0.0'
+
+configure do
+  db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'WorkersLU', :user => 'superuser', :password => 'password')  
+  set :mongo_db, db[:WorkersLU]
+end
+
+# By default Sinatra will return the string as the response.
+get '/hello-world' do
+  "Hello World!"
+end
+
+get '/?' do
+  content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @employees = settings.mongo_db.database.collection('Penality').find
+end
+
+get '/penalty/:id' do
+  # content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @penalties = settings.mongo_db.database.collection('Penality').find({"employee_id" => params[:id].to_i})
+  erb :penalties
+end
+
+get '/office/:id' do
+  # content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @offices = settings.mongo_db.database.collection('Office').find({"id" => params[:id].to_i})
+  erb :office
+end
+
+get '/events/:id' do
+  # content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @events = settings.mongo_db.database.collection('Event').find({"employee_id" => params[:id].to_i})
+  erb :event
+end
+
+get '/event_by_id/:id' do
+  # content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @events = settings.mongo_db.database.collection('Event').find({"id" => params[:id].to_i})
+  erb :event
+end
+
+get '/master/:id' do
+  # content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @masters = settings.mongo_db.database.collection('Master').find({"employee_id" => params[:id].to_i})
+  erb :master
+end
+
+get '/master_by_id/:id' do
+  # content_type :json
+  # settings.mongo_db.database.collection_names.to_json
+  @masters = settings.mongo_db.database.collection('Master').find({"id" => params[:id].to_i})
+  erb :master
+end
+
+get '/employee_by_id/:id' do
+  # content_type :json
+  @employees = settings.mongo_db.database.collection('Employee').find({"id" => params[:id].to_i})
+  erb :employee
+end
+
+
+get '/employee/?' do
+  # content_type :json
+  @employees = settings.mongo_db.database.collection('Employee').find
+  erb :employee
+end
